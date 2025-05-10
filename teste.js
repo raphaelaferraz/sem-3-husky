f27f9a18a7843d8484e664620a68a135734bb4de
raphaelaferraz <raphaelagferraz2@gmail.com>
2025-05-09 21:36:53 -0300

    feat: add configuração completa, testes e relatório

--- teste.js
+++ teste.js
@@ -1,1 +1,14 @@
 console.log('Se chegou aqui, é porque o Husky está funcionando!');
+
+// Função que soma dois números
+function soma(a, b) {
+  return a + b;
+}
+
+// Teste da função
+console.log('Teste da função soma:');
+console.log('2 + 3 =', soma(2, 3));
+console.log('5 + 7 =', soma(5, 7));
+
+// Exporta a função para testes
+module.exports = { soma };
---
+++ teste.test.js
@@ -0,0 +1,11 @@
+const { soma } = require('./teste');
+
+describe('Testes da função soma', () => {
+  test('soma 2 + 3 deve ser igual a 5', () => {
+    expect(soma(2, 3)).toBe(5);
+  });
+
+  test('soma 5 + 7 deve ser igual a 12', () => {
+    expect(soma(5, 7)).toBe(12);
+  });
+});
